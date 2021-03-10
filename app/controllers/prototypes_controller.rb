ffd09fef72a3cf89730ca506fb52d7d5a1d0b571
yuma <yuma.4831@gmail.com>
2021-03-10 13:15:03 +0900

    3/8指摘部分修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,7 +20,6 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[ :id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
